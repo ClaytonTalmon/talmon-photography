b5fa49329448aafeabccca4b255405a5f1bcf06f
Clayton Talmon <134451273+ClaytonTalmon@users.noreply.github.com>
2026-09-25 19:16:44 +0200

    Shorten navigation label to Appointments

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -148,7 +148,7 @@ export const translations: Record<Locale, Dictionary> = {
       work: 'Work',
       studio: 'Studio',
       about: 'About',
-      privateView: 'Discuss the Collection',
+      privateView: 'Appointments',
       contact: 'Contact',
     },
     footer: {
